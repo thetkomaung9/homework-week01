43b2745c8c58459a536e03e468d639b38aa4f467
thetko1996 <thetkomaung966@gmail.com>
2025-09-06 03:19:55 +0900

    Fix: Print specific value from map in 1_1_map_type.dart
    This commit modifies the main function in 1_1_map_type.dart to print a specific value ('Harry Poter') from the dictionary map.

--- lib/week01/1.4.2/1_1_map_type.dart
+++ lib/week01/1.4.2/1_1_map_type.dart
@@ -1,7 +1,8 @@
-void main(){
+void main() {
   Map<String, String> dictionary = {
-    'Harry Poter' : '해리포터',
-    'Ron Weasley' : '론 위즐리',
-    'Hermione Granger' : '헤르미온느 그레인저'
+    'Harry Poter': '해리포터',
+    'Ron Weasley': '론 위즐리',
+    'Hermione Granger': '헤르미온느 그레인저',
   };
-  print(dictionary)}
+  print(dictionary['Harry Poter']);
+}
