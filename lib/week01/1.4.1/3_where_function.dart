af8aad032bf19d375ebf6e9f2e7de09e6e0625e8
thetko1996 <thetkomaung966@gmail.com>
2025-09-06 02:45:25 +0900

    feat(week01): Add where function example with filter comment

--- lib/week01/1.4.1/3_where_function.dart
+++ lib/week01/1.4.1/3_where_function.dart
@@ -2,6 +2,6 @@ void main() {
   List<String> blackPinkList = ['리사', '지수', '제니', '로제'];
 
   final newList = blackPinkList.where(
-      (name) => name == '리사' || name == '지수' //
+      (name) => name == '리사' || name == '지수' //'리사' 또는 '지수'만 유지
   )
 }
