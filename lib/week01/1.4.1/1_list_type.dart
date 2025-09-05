665bda262457fcc0d1b61b7fc2b32b5f44b5b6ea
thetko1996 <thetkomaung966@gmail.com>
2025-09-06 02:34:25 +0900

    feat: Add basic list usage example
    Demonstrates how to declare and access elements in a List<String>.

--- lib/week01/1.4.1/1_list_type.dart
+++ lib/week01/1.4.1/1_list_type.dart
@@ -1,3 +1,7 @@
 void main() {
-  //리스트에 넣을 타일을 <> 사이에 명서
+  //리스트에 넣을 타일을 <> 사이에 명시할 수 있습니다.
+  List<String> blackPinkList = ['리사', '지수', '제니', '로제'];
+
+  print(blackPinkList);
+  print(blackPinkList[0]); //
 }
