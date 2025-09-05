c781b8a0e297db54b59ad7c4bf8d6b62f3bbb15c
thetko1996 <thetkomaung966@gmail.com>
2025-09-06 02:18:55 +0900

    Fix: Clarify const variable immutability in example
    This commit clarifies that const variables cannot be changed after initialization.

--- lib/week01/1.3/6_1_final_and_const.dart
+++ lib/week01/1.3/6_1_final_and_const.dart
@@ -3,5 +3,5 @@ void main(){
   name ='BTS'; //에러 발생. final로 선언한 변수는 선언 후 값을 변경할 수 없음
 
   const String name2 = 'BTS';
-  name2 = '블랙핑크'; //에러 발생. const로 선언한
+  name2 = '블랙핑크'; //에러 발생. const로 선언한 변수는 선언 후 값을 변경할 수 없음
 }
