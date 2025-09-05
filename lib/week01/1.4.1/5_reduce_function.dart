cd4840332705b0e593a8d46ee0966efa177df465
thetko1996 <thetkomaung966@gmail.com>
2025-09-06 03:00:55 +0900

    Fix: Corrected syntax error in reduce function
    The  function was missing its function body, causing a syntax error.  This commit adds an empty function body () to resolve the issue.

--- lib/week01/1.4.1/5_reduce_function.dart
+++ lib/week01/1.4.1/5_reduce_function.dart
@@ -1,3 +1,3 @@
 void main() {
   List<String> blackPinkList = ['리사', '지수', '제니', '로제'];
-final allMenmabers = blackPinkList.reduce((value , element))}
+final allMenmabers = blackPinkList.reduce((value , element) => )}
