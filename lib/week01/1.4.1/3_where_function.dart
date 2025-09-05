fa25bd7bc27d67b270191cb94e69714df57fe797
thetko1996 <thetkomaung966@gmail.com>
2025-09-06 02:44:55 +0900

    Fix: Implement where function example in 3_where_function.dart
    This commit completes the example by adding a filter condition to the  function, selecting '리사' and '지수' from the list.

--- lib/week01/1.4.1/3_where_function.dart
+++ lib/week01/1.4.1/3_where_function.dart
@@ -1,5 +1,7 @@
 void main() {
   List<String> blackPinkList = ['리사', '지수', '제니', '로제'];
 
-  final newList = blackPinkList.where((element) => ,)
+  final newList = blackPinkList.where(
+      (name) => name == '리사' || name == '지수' //
+  )
 }
