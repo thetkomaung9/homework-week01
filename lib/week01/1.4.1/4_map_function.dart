e45b84d07d7a62cbc9c4540546b43d9e6af659d9
thetko1996 <thetkomaung966@gmail.com>
2025-09-06 02:53:55 +0900

    Fix: Corrected typo in comment for map function example.

--- lib/week01/1.4.1/4_map_function.dart
+++ lib/week01/1.4.1/4_map_function.dart
@@ -2,7 +2,7 @@ void main(){
   List<String> blackPinkList = ['리사', '지수', '제니', '로제'];
 
   final newBlackPink = blackPinkList.map(
-    (name) => '불랙핑크 $name'; //리스트의 모든 값 앞에 '블랙핑크'
+    (name) => '불랙핑크 $name', //리스트의 모든 값 앞에 '블랙핑크'
 추가  );
 
 print(newBlackPink);
