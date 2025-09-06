158bd2192e07485a758f2d4840328d1ea40201ae
thetko1996 <thetkomaung966@gmail.com>
2025-09-07 00:16:03 +0900

    feat(week01): Add comparison operator examples

--- lib/week01/1.5/3_value_comparison_operators.dart
+++ lib/week01/1.5/3_value_comparison_operators.dart
@@ -3,4 +3,9 @@ void main() {
   int number2 = 2;
 
   print(number1 > number2); //false
+  print(number1 < number2); //true
+  print(number1 >= number2); //false
+  print(number1 <= number2); //true
+  print(number1 == number2); //false
+  print(number1 != number2); //true
 }
