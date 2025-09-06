da738708b01bdb59c98e01467bf809288d9c7356
thetko1996 <thetkomaung966@gmail.com>
2025-09-06 23:47:03 +0900

    Fix: Convert List to Set and demonstrate
    This commit adds an example demonstrating how to convert a List to a Set using .  It also fixes a minor typo in the original file.

--- lib/week01/1.4.3/1_set_type.dart
+++ lib/week01/1.4.3/1_set_type.dart
@@ -5,6 +5,7 @@ void main(){
   print(blackPink.contains('로제')); //2 값이 있는지 확인하기
   print(blackPink.toList()); //3리스트로 변환하기
 
-  List<String> blackPink2 = []
+  List<String> blackPink2 = ['로제' , '지수' , '지수'];
+  print(Set.form(blackPink2)); //4 List 타입을 Set 타입으로 변환
 
 }
