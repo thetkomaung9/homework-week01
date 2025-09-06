8c1a0e9337955aafd6915d16341d5efb2ca61bdc
thetko1996 <thetkomaung966@gmail.com>
2025-09-07 01:47:04 +0900

    Fix: Add empty catch block to try-catch example
    The try-catch example was missing the  block.  This commit adds an empty catch block to ensure the example functions correctly without causing an unhandled exception error.

--- lib/week01/1.8/1_1_try_catch.dart
+++ lib/week01/1.8/1_1_try_catch.dart
@@ -1,9 +1,10 @@
-void main(){
+void main() {
   try {
-
     //에러가 없을 때 실행할 로직
-    final String name ='코드팩토리';
+    final String name = '코드팩토리';
 
     print(name); //1에러가 없으니 출럭됨
+  } catch (e) {
+    //catch
   }
 }
