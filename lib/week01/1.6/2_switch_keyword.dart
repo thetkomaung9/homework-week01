ccc302e7ab15f93ce2cd6d78b0de1b31d58fada9
thetko1996 <thetkomaung966@gmail.com>
2025-09-07 00:39:33 +0900

    Refactor: Update switch statement output for clarity
    Updates the printed messages within the switch statement for better readability and context.

--- lib/week01/1.6/2_switch_keyword.dart
+++ lib/week01/1.6/2_switch_keyword.dart
@@ -6,10 +6,10 @@ void main() {
   switch (status) {
     case Status.approved:
       //approved값이기 때문에 다음 코드가 실행됩니다.
-      print("승인됨");
+      print("승인 상태입니다.");
       break;
     case Status.pending:
-      print("대기중");
+      print("대기 상태입니다.");
       break;
     case Status.rejected:
       print("거절됨");
