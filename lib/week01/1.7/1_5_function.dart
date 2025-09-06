f032039b7342da9882ae7a8cff9e1e32b53d33df
thetko1996 <thetkomaung966@gmail.com>
2025-09-07 01:16:03 +0900

    Fix: Update addTwoNumbers example with named parameter 'c'
    This commit updates the example in  to demonstrate the usage of the named parameter 'c' in the  function.

--- lib/week01/1.7/1_5_function.dart
+++ lib/week01/1.7/1_5_function.dart
@@ -1,11 +1,7 @@
-int addTwoNumbers(
-  int a, {
-    required int b,
-    int c = 3,
-  }) {
+int addTwoNumbers(int a, {required int b, int c = 3}) {
   return a + b + c;
-  }
+}
+
 void main() {
-  print(addTwoNumbers(1, b: 2));
-  }
-)
+  print(addTwoNumbers(1, b: 3, c: 7));
+}
