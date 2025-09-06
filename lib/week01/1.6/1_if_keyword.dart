8a733962f6386c7d08313dc28ee5eaefde94b0ba
thetko1996 <thetkomaung966@gmail.com>
2025-09-07 00:31:03 +0900

    feat(week01): Add example demonstrating the 'if' keyword

---
+++ lib/week01/1.6/1_if_keyword.dart
@@ -0,0 +1,11 @@
+void main() {
+  int number = 2;
+
+  if (number % 3 == 0) {
+    print('3의 배수입니다.');
+  } else if (number % 2 == 0) {
+    print('2의 배수입니다.');
+  } else {
+    print('3의 배수도, 2의 배수도 아닙니다.');
+  }
+}
