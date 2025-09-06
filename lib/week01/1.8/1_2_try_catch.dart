ec26b2d99d0ebf1bef143f51a40b57caef13ad42
thetko1996 <thetkomaung966@gmail.com>
2025-09-07 01:53:34 +0900

    feat(week01): Introduce try-catch and exception handling example
    This commit adds a basic try-catch block with exception throwing for demonstration purposes in week01/1.8.

--- lib/week01/1.8/1_2_try_catch.dart
+++ lib/week01/1.8/1_2_try_catch.dart
@@ -1,6 +1,12 @@
-void main(){
-  try{
+void main() {
+  try {
     final String name = '코드팩토리';
-    )
+
+    //1throw 키위드로 고의적으로 에러를 발생시킵니다.
+    throw Exception('이름이 잘못됐습니다!');
+
+    print(name);
+  } catch (e) {
+    print(e);
   }
 }
