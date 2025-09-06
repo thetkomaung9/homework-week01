9ada711d758f69ec228b8b14e58e312d4bcf3d09
thetko1996 <thetkomaung966@gmail.com>
2025-09-06 23:48:33 +0900

    Fix: Correct typo and spacing in set_type.dart
    Addresses minor formatting and spacing issues in the set_type.dart file.

--- lib/week01/1.4.3/1_set_type.dart
+++ lib/week01/1.4.3/1_set_type.dart
@@ -1,11 +1,10 @@
-void main(){
-  Set<String> blackPink = {'로제' , '지수' , '리사' , '제니' , '제니'} //1 제니 중복
+void main() {
+  Set<String> blackPink = {'로제', '지수', '리사', '제니', '제니'}; //1 제니 중복
 
   print(blackPink);
   print(blackPink.contains('로제')); //2 값이 있는지 확인하기
   print(blackPink.toList()); //3리스트로 변환하기
 
-  List<String> blackPink2 = ['로제' , '지수' , '지수'];
+  List<String> blackPink2 = ['로제', '지수', '지수'];
   print(Set.form(blackPink2)); //4 List 타입을 Set 타입으로 변환
-
 }
