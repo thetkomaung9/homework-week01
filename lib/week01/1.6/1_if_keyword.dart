b64e8914ceff43b464dd40ec4fa0b61b6f451e28
thetko1996 <thetkomaung966@gmail.com>
2025-09-07 00:31:33 +0900

    Fix: Corrected incorrect condition in if-else statement
    The  condition was incorrectly checking for divisibility by 2. This commit corrects it to check if the remainder after division by 3 is 1.

--- lib/week01/1.6/1_if_keyword.dart
+++ lib/week01/1.6/1_if_keyword.dart
@@ -4,7 +4,7 @@ void main() {
   if (number % 3 == 0) {
     print('3의 배수입니다.');
   } else if (number % 2 == 0) {
-    print('2의 배수입니다.');
+    print('나머지가 1입니다.');
   } else {
     print('3의 배수도, 2의 배수도 아닙니다.');
   }
