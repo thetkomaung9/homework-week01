ad74988e7a3e46d3f4b732e179d795d99b62f3da
thetko1996 <thetkomaung966@gmail.com>
2025-09-14 00:12:58 +0900

    Fix: Destructure 'name' from map correctly
    The previous code attempted to destructure the entire map, not just the 'name' key. This commit fixes it by correctly destructuring the 'name' key into a variable named 'name'.

--- lib/week02/4.2/3.dart
+++ lib/week02/4.2/3.dart
@@ -1,6 +1,6 @@
 void main() {
   final minjiMap = {'name': '민지', 'age': 19};
-  final {'name': '민지', 'age': 19} = minjiMap;
+  final {'name': name, 'age': 19} = minjiMap;
 
   print('name: $name');
   print('age: $age');
