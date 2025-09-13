2f75a5dbcf63dc8d0b31ecaa75ac0d91ede5ae62
thetko1996 <thetkomaung966@gmail.com>
2025-09-14 00:42:58 +0900

    Fix: Correctly handle switch case with wildcard and when clause
    This commit addresses an issue in the switch statement logic where a wildcard and when clause were not being evaluated correctly. The code now prints 'default' as expected because  is -1, not greater than 0.

--- lib/week02/4.3/4.dart
+++ lib/week02/4.3/4.dart
@@ -1,1 +1,11 @@
-void main() {}
+void main() {
+  (int a, int b) val = (1, -1);
+
+  switch (val) {
+    case (1, _) when val.$2 > 0:
+      print('1, _');
+      break;
+    default:
+      print('default');
+  }
+}
