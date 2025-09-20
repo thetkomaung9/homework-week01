d31e5b6546ad7f6bef00f8748c7a69c96d9b43dc
thetko1996 <thetkomaung966@gmail.com>
2025-09-21 02:43:41 +0900

    Fix: Remove unnecessary comments from TextButton widget
    Removes redundant comments from the TextButton widget for cleaner code.

--- lib/week03/text_button_widget.dart
+++ lib/week03/text_button_widget.dart
@@ -1,24 +1,23 @@
 import 'package:flutter/material.dart';
 
-void main() {
+void main(){
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget{
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child:TextButton(
-            // 클릭 시 실행
+          child: TextButton(
             onPressed: () {},
-            // 스타일 지정
+
             style: TextButton.styleFrom(
-              // 주색상 지정
+
               foregroundColor: Colors.red,
             ),
-            // 버튼에 넣을 위젯
+
             child: Text('텍스트 버튼'),
           ),
         ),
