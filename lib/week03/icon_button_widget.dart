84dfaf6d2f285337f3eef56eeba3ae768954bdea
thetko1996 <thetkomaung966@gmail.com>
2025-09-21 03:06:11 +0900

    feat: Add home icon to IconButtonWidget
    This commit adds the home icon to the IconButtonWidget in week03.

--- lib/week03/icon_button_widget.dart
+++ lib/week03/icon_button_widget.dart
@@ -14,9 +14,11 @@ class MyApp extends StatelessWidget{
             onPressed:(){},
             icon: Icon(
               //플러터에서 기본으로 제공하는 아이콘입니다.
-              //
-            )
-          )
+              //제공되는 아이콘 목록은 다음 링크에서 확인해볼 수 있습니다.\
+              //https://fonts.google.com/icons
+              Icons.home,
+            ),
+          ),
         ),
       ),
     );
