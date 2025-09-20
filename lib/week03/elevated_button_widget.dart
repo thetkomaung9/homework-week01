4464716036e01181f99bb596152f4fbed8d63bad
thetko1996 <thetkomaung966@gmail.com>
2025-09-21 02:55:11 +0900

    feat: Add basic elevated button widget structure
    This commit introduces a basic structure for an elevated button widget, including necessary imports and a minimal UI.

---
+++ lib/week03/elevated_button_widget.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/week02/6/main.dart';
+
+void main(){
+  runApp(MyApp());
+
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          
+        ),
+      ),
+    )
+  }
+}
