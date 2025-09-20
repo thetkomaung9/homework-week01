31e48772fd6304a5959e7136bf552ea1154f23d8
thetko1996 <thetkomaung966@gmail.com>
2025-09-21 03:20:41 +0900

    feat: Implement a basic FloatingActionButton example

--- lib/week03/floating_action_button_widget.dart
+++ lib/week03/floating_action_button_widget.dart
@@ -4,3 +4,19 @@ void main(){
   runApp(FloatingActionButtonExample());
 
 }
+
+class FloatingActionButtonExample extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+
+          onPressed: () {},
+          child: Text('클릭'),
+          
+        ),
+      )
+    )
+  }
+}
