e259d99d4398eddfb0ffcac21c0fff720a06c209
thetko1996 <thetkomaung966@gmail.com>
2025-09-22 01:08:33 +0900

    feat: Add basic structure for PaddingMarginWidget
    This commit introduces the initial file with basic structure and imports necessary for the PaddingMarginWidget.

---
+++ lib/week03/padding_margin_widget.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/week03/text_button_widget.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+
+class MyApp extends StatelessWidget{
+  @override
+
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: ,
+        ),
+      )
+    )
+  }
+}
