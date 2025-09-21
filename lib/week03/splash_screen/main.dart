f2c750c4393b396e5ce9c39ad0510f84c3d74c0f
thetko1996 <thetkomaung966@gmail.com>
2025-09-22 02:04:35 +0900

    feat: Implement basic splash screen UI

--- lib/week03/splash_screen/main.dart
+++ lib/week03/splash_screen/main.dart
@@ -7,6 +7,13 @@ void main(){
 class SplashScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context){
-    
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+
+          child: Text('Splash Screen'),
+        ),
+      ),
+    )
   }
 }
