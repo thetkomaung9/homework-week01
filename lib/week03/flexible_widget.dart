aa218f3e6cbf699d371b0bca4fe110967bb3bbc0
thetko1996 <thetkomaung966@gmail.com>
2025-09-22 01:41:03 +0900

    feat: Add basic Flexible Widget structure

---
+++ lib/week03/flexible_widget.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/week03/container_widget.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: ,
+        ),
+      )
+    )
+  }
+}
