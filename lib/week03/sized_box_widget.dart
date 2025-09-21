5f2414ba67f017d22fe5f65223ca4f444c5b123d
thetko1996 <thetkomaung966@gmail.com>
2025-09-22 00:53:03 +0900

    feat(week03): Implement SizedBox widget example
    Adds a SizedBox widget example to demonstrate its usage.

--- lib/week03/sized_box_widget.dart
+++ lib/week03/sized_box_widget.dart
@@ -12,11 +12,16 @@ class MyApp extends StatelessWidget{
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: 
+          child: SizedBox(
+            height: 200.0,
+            width: 200.0,
+            child: Container(
+              color: Colors.red,
             ),
-          ),
+          )
+            
         ),
       ),
-    )
+    );
   }
 }
