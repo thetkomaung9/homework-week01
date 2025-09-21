926b208ec2bfd57d68bb22d7622db69962ca395c
thetko1996 <thetkomaung966@gmail.com>
2025-09-22 00:51:33 +0900

    Fix: Correctly implement Center widget with child in SizedBox example.

--- lib/week03/sized_box_widget.dart
+++ lib/week03/sized_box_widget.dart
@@ -12,7 +12,9 @@ class MyApp extends StatelessWidget{
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          
+          child: 
+            ),
+          ),
         ),
       ),
     )
