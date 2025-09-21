6b9992c14c2357dc1a68de41e5b07bc70ebaa2ee
thetko1996 <thetkomaung966@gmail.com>
2025-09-22 03:06:34 +0900

    feat: Implement basic home screen layout

--- lib/week03/blog_web_app/screen/home_screen.dart
+++ lib/week03/blog_web_app/screen/home_screen.dart
@@ -1,40 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
-
 class HomeScreen extends StatelessWidget{
-   WebViewController webViewController = WebViewController()
 
-   ..loadRequest(Uri.parse('https://blog.codefactory.ai'))
+  const HomeScreen({Key? key}) : super(key: key);
 
-   ..setJavaScriptMode(JavaScriptMode.unrestricted);
-
-   HomeScreen({Key? key}) : super(key: key);
-
-   @override
-   Widget build(BuildContext context){
+  @override
+  Widget build(BuildContext context){
     return Scaffold(
-
-      appBar: AppBar(
-
-        backgroundColor: Colors.orange,
-
-        title: Text('Code Factory'),
-        centerTitle: true,
-
-        actions: [
-          IconButton(
-            onPressed: (){
-              webViewController.loadRequest(Uri.parse('https://blog.codefactory.ai'));
-            },
-            icon: Icon(
-              Icons.home,
-            ),
-          ),
-        ],
-      ),
-      body: WebViewWidget(
-        controller: webViewController,
-      ),
+      body: Text('Home Screen'),
     );
-   }
+  }
 }
