0508be4a76e1cf3f07baa21711fc58171d5e7ffd
thetko1996 <thetkomaung966@gmail.com>
2025-09-22 00:41:03 +0900

    Fix: Correct typo in Container widget instantiation.

--- lib/week03/container_widget.dart
+++ lib/week03/container_widget.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget{
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: ,
+          child: Contai,
         ),
       ),
     )
