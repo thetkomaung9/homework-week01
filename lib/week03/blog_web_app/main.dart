309e5730de8cb85279e4862a920af96a34335332
thetko1996 <thetkomaung966@gmail.com>
2025-09-22 02:39:04 +0900

    feat: Implement basic MaterialApp setup with HomeScreen
    This commit sets up the basic structure for the blog web app by initializing the MaterialApp widget and setting the HomeScreen as the home route.

--- lib/week03/blog_web_app/main.dart
+++ lib/week03/blog_web_app/main.dart
@@ -1,1 +1,10 @@
 import 'package:myapp/week03/blog_web_app/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    ),
+  );
+}
