630b7cc5e0a51355dd6b862535df8a5353c957df
thetko1996 <thetkomaung966@gmail.com>
2025-09-22 01:47:04 +0900

    feat: Add basic Expanded Widget scaffold
    This commit creates a basic scaffold for the Expanded Widget example in week03, including a MaterialApp, Scaffold, and a simple Column in the body.

---
+++ lib/week03/expanded_widget.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/week03/icon_button_widget.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            children: [],
+          ),
+        ),
+      ),
+    )
+  }
+
+}
