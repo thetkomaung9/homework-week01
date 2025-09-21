0eb28b44cd6c91b64ef322deff2943158b6c91c6
thetko1996 <thetkomaung966@gmail.com>
2025-09-22 02:03:34 +0900

    feat: Implement basic SplashScreen widget
    Adds a basic SplashScreen widget with an empty build method.

---
+++ lib/week03/splash_screen/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(SplashScreen());
+}
+
+class SplashScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    
+  }
+}
