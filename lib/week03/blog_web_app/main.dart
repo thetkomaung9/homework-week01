80e856204e0e9b52f24d25ab1e2f1e31e5dd8935
thetko1996 <thetkomaung966@gmail.com>
2025-09-22 02:59:04 +0900

    Fix: Correct import path for HomeScreen
    The import path for HomeScreen was incorrect, causing the app to fail to load. This commit corrects the path.

--- lib/week03/blog_web_app/main.dart
+++ lib/week03/blog_web_app/main.dart
@@ -1,10 +1,10 @@
-import 'package:myapp/week03/blog_web_app/screen/home_screen.dart';
-import 'package:flutter/material.dart';
+import'package:week03/week03/blog_web_app/screen/home_screen.dart';
+import'package:flutter/material.dart';
 
 void main(){
   runApp(
     MaterialApp(
       home: HomeScreen(),
-    ),
+  ),
   );
 }
