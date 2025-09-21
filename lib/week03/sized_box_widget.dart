d92f13d72d37888eadb85e76e0b121131042b8dd
thetko1996 <thetkomaung966@gmail.com>
2025-09-22 00:50:33 +0900

    feat: Add basic MyApp widget structure
    This commit adds the basic structure for the MyApp widget, including MaterialApp and Scaffold.

--- lib/week03/sized_box_widget.dart
+++ lib/week03/sized_box_widget.dart
@@ -4,3 +4,17 @@ import 'package:flutter/material.dart';
 void  main(){
   runApp(MyApp());
 }
+
+class MyApp extends StatelessWidget{
+  @override
+
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          
+        ),
+      ),
+    )
+  }
+}
